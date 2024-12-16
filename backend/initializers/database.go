80a6e7143f5a8c65491277e35be93f3ca93ec237
Taielmolina01 <tamolina@fi.uba.ar>
2024-12-16 18:03:30 -0300

    connecting to db and creating models

---
+++ backend/initializers/database.go
@@ -0,0 +1,55 @@
+package initializers
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"fmt"
+    "os"
+)
+
+var DB *gorm.DB
+
+const (
+	MESSAGE_ERROR_CONNECTING_DB = "Error connecting to the database"
+	MESSAGE_ERROR_CREATING_DB = "Database already exists or had ocurred an error creating it"
+	MESSAGE_SUCCESS_CREATING_DB = "Success creating the database"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+    err := LoadEnvVariables()
+	if err != nil {
+		return nil, err
+	}
+
+	dsn := os.Getenv("DB_DSN")
+    
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal(MESSAGE_ERROR_CONNECTING_DB, err)
+    }
+
+	createDatabaseIfNotExist(dsn)
+
+	db, err = gorm.Open(postgres.Open(dsn+" dbname=movie-system-db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(MESSAGE_ERROR_CONNECTING_DB, err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func createDatabaseIfNotExist(dsn string) {
+    conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal(MESSAGE_ERROR_CONNECTING_DB, err)
+    }
+
+    err = conn.Exec("CREATE DATABASE movie-system-db").Error
+    if err != nil {
+        fmt.Println(MESSAGE_ERROR_CREATING_DB, err)
+    } else {
+        fmt.Println(MESSAGE_SUCCESS_CREATING_DB)
+    }
+}
---
+++ backend/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"movie-reservation-system/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func (c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to my movie reservation system",
+		})
+	})
+
+	r.Run()
+}
