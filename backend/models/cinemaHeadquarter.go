85da9b94d65478aab7fd62a516f820fbf7b7f56c
Taielmolina01 <tamolina@fi.uba.ar>
2024-12-17 17:18:11 -0300

    correct a wrong comment

--- backend/models/cinemaHeadquarter.go
+++ backend/models/cinemaHeadquarter.go
@@ -16,7 +16,7 @@ type CinemaHeadquarterDB struct {
 	CinemaChainID uuid.UUID `gorm:"type:uuid;not null"`
 	// Declare the relationship CinemaHeadquarter - CinemaChain.
 	// If the CinemaChainID is updated, it will be updated in this table. 
-	// By the other hand if I want to delete a CinemaChain having CinemaHeadquarters, i won't can do it.
+	// By the other hand if I want to delete a CinemaChain having CinemaHeadquarters, i won't be able to do it.
 	CinemaChain CinemaChainDB `gorm:"foreignKey:CinemaChainID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 	// FK
 	HeadquarterAdressID uuid.UUID `gorm:"type:uuid;not null`
