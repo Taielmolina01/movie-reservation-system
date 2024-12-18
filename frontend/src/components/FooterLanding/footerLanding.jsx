d22aeb57a00e54db1043d2ef7cae5cee96a5a98c
Taielmolina01 <tamolina@fi.uba.ar>
2024-12-18 17:06:47 -0300

    adding skeleton of landing

---
+++ frontend/src/components/FooterLanding/footerLanding.jsx
@@ -0,0 +1,11 @@
+import './footerLanding.css'
+
+function FooterLanding() {
+    return (
+        <footer className='footer-landing'>
+            <h1>CinemaChainName</h1>
+        </footer>
+    )
+}
+
+export default FooterLanding;
