0c5ccfdeba277e387ae49e8886d58adac07944b6
Taielmolina01 <tamolina@fi.uba.ar>
2024-12-19 17:50:19 -0300

    all declared and well compiled for the userController

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"movie-reservation-system/initializers"
 )
 
+var DB *gorm.DB
+
 func init() {
 	initializers.LoadEnvVariables()
-	initializers.ConnectDB()
+	DB, _ = initializers.ConnectDB()
 }
 
 func main() {
 	r := gin.Default()
+
+	userController := initializers.Init(DB)
+
+	initializers.SetUpRoutes(r, userController)
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
